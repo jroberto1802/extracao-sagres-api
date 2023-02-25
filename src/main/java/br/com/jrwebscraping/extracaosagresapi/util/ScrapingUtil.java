99c44c15b4b176ac4a90b81160b9c30426a8da25
Roberto Junior <jroberto802@gmail.com>
2023-02-25 15:43:31 -0300

    Criado DTO com dados de partidas, criado enum de status da partida e alguns ajustes

---
+++ src/main/java/br/com/jrwebscraping/extracaosagresapi/util/ScrapingUtil.java
@@ -0,0 +1,13 @@
+package br.com.jrwebscraping.extracaosagresapi.util;
+
+public class ScrapingUtil {
+	
+	private static final String BASE_URL_GOOGLE = "http://www.goole.com.br/search?q=";
+	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/br/com/jrwebscraping/extracaosagresapi/util/StatusPartida.java
@@ -0,0 +1,10 @@
+package br.com.jrwebscraping.extracaosagresapi.util;
+
+public enum StatusPartida {
+	
+	PARTIDA_NAO_INICIADA,
+	PARTIDA_EM_ANDAMENTO,
+	PARTIDA_ENCERRADA,
+	PARTIDA_COM_PENALIDADES;
+	
+}
