550928b86eb7512deb466c76fe6e7110e18bb274
Roberto Junior <jroberto802@gmail.com>
2023-02-24 22:45:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/jrwebscraping/extracaosagresapi/ExtracaoSagresApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jrwebscraping.extracaosagresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracaoSagresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracaoSagresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jrwebscraping/extracaosagresapi/ExtracaoSagresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jrwebscraping.extracaosagresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtracaoSagresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
