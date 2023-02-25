1c687dc44b20c435f5eecea555346e4adf92b5e5
Roberto Junior <jroberto802@gmail.com>
2023-02-25 16:09:00 -0300

    Conectando com jsoup no google

--- src/main/java/br/com/jrwebscraping/extracaosagresapi/util/ScrapingUtil.java
+++ src/main/java/br/com/jrwebscraping/extracaosagresapi/util/ScrapingUtil.java
@@ -1,13 +1,44 @@
 package br.com.jrwebscraping.extracaosagresapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.jrwebscraping.extracaosagresapi.dto.PartidaGoogleDTO;
+
 public class ScrapingUtil {
 	
-	private static final String BASE_URL_GOOGLE = "http://www.goole.com.br/search?q=";
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+	private static final String BASE_URL_GOOGLE = "http://www.google.com.br/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+		
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformcoesPartida(url);
+	}
+	
+	public PartidaGoogleDTO obtemInformcoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		
+		Document document = null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			
+			String title = document.title();
+			LOGGER.info("Titulo da pagina: {}", title);
+			
+			
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
+		}
+		
+		return partida;
 	}
 
 }
